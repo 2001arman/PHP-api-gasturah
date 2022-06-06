15d65cff2bf58f24a0fd6bf0dc8829ae7dfdccdf
Arman <2001armanmaulana@gmail.com>
2022-06-07 00:06:29 +0800

    new response when duplicate username

--- register.php
+++ register.php
@@ -14,20 +14,29 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     $file_tmp = $_FILES['profile_picture']['tmp_name'];
 
     $image_name = $username . ".$ekstensi";
-
-    move_uploaded_file($file_tmp, 'img/'.$image_name);
-
+    
     $query_insert= "INSERT INTO `user` SET `username`='$username',`password`='$password', `name`='$name', profile_picture='img/$image_name'";
-   $hasil      = mysqli_query($con, $query_insert);
+    $hasil      = mysqli_query($con, $query_insert);
+    
    if(!$hasil){
-       printf("error : %s\n", mysqli_error($con));
-       exit();
+       if(mysqli_error($con) == "Duplicate entry '$username' for key 'username'"){
+            $response['value'] = '200';
+            $response['status'] = 'failed';
+            $response['msg'] = "Username $username sudah digunakan";
+            $response['content'] = (object)[];
+            echo json_encode($response);
+            exit();
+       } else{
+           printf("error : %s\n", mysqli_error($con));
+           exit();
+       }
    }
     if(isset($hasil)){
+        move_uploaded_file($file_tmp, 'img/'.$image_name);
 	     $response['value'] = '200';
 	     $response['status'] = 'success';
 	     $response['msg'] = 'Berhasil mendaftarkan akun';
-	     $response['content'] = "kosong";
+	     $response['content'] = (object)[];
         echo json_encode($response);
     }else{
         $response['value'] = '0';
