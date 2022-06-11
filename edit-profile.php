5d4e08274a3391f8e0c0b31deca0feca1c3f0275
Arman <2001armanmaulana@gmail.com>
2022-06-11 18:03:52 +0800

    fix api edit profile

---
+++ edit-profile.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once "connect.php";
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $response   = array();
+    $username_awal   = $_POST['username_awal'];
+    $username_baru   = $_POST['username_baru'];
+    $name   = $_POST['nama'];
+    $password   = $_POST['password'];
+    $foto = $_POST['profile_picture'];
+    $hasilImage = base64_decode($foto);
+
+
+    // $nama = $_FILES['profile_picture']['name'];
+    // $x = explode('.', $nama);
+    // $ekstensi = strtolower(end($x));
+    // $file_tmp = $_FILES['profile_picture']['tmp_name'];
+
+    $image_name = $username_baru . ".png";
+
+    $query      = "UPDATE user SET `username`='$username_baru', `password`='$password', `name`='$name', profile_picture='img/$image_name' WHERE  `username` = '$username_awal'";
+    $hasil      = mysqli_query($con, $query);
+
+    if(!$hasil){
+        if(mysqli_error($con) == "Duplicate entry '$username_baru' for key 'username'"){
+             $response['value'] = '200';
+             $response['status'] = 'failed';
+             $response['msg'] = "Username $username_baru sudah digunakan";
+             $response['content'] = (object)[];
+             echo json_encode($response);
+             exit();
+        } else{
+            printf("error : %s\n", mysqli_error($con));
+            exit();
+        }
+    }
+     if(isset($hasil)){
+        //  move_uploaded_file($file_tmp, 'img/'.$image_name);
+        file_put_contents('img/'.$image_name, $hasilImage);
+         $query      = "SELECT * FROM `user` WHERE `username` ='$username_baru'";
+         $hasil      = mysqli_query($con, $query);
+         while ($fetchdata = mysqli_fetch_array($hasil)) {
+            $response['value'] = '200';
+            $response['status'] = 'success';
+            $response['msg'] = 'Berhasil login';
+            $response['content'] = [
+                'username' => $fetchdata['username'],
+                'name' => $fetchdata['name'],
+                'profile_picture' => $fetchdata['profile_picture'],
+                'level' => $fetchdata['level'],
+            ];
+        }
+         echo json_encode($response);
+     }else{
+         $response['value'] = '0';
+         echo json_encode($response);
+     }
+
+}
