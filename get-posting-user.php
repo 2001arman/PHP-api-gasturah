d2df8ba801212490b26ad8c5cf9ba90491353163
Arman <2001armanmaulana@gmail.com>
2022-06-12 13:51:25 +0800

    get posting user api

---
+++ get-posting-user.php
@@ -0,0 +1,34 @@
+    <?php
+
+    require_once "connect.php";
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        $response   = array();
+        $username   = $_POST["username"];
+        $query      = "SELECT * FROM `postingan` INNER JOIN user ON postingan.user = user.username WHERE `user` = '$username'";
+        $hasil      = mysqli_query($con, $query);
+
+        if(mysqli_num_rows($hasil)===0){
+            $response['value'] = '200';
+            $response['status'] = 'failed';
+            $response['msg'] = 'Data postingan masih kosong';
+            $response['content'] = (object)[];
+
+            echo json_encode($response);
+
+        } else{
+            $response['value'] = '200';
+            $response['status'] = 'success';
+            $response['msg'] = 'Berhasil mengambil data postingan';
+            while ($fetchdata = mysqli_fetch_array($hasil)) {
+                $response['content'][] = [
+                    'user' => $fetchdata['user'],
+                    'foto' => $fetchdata['foto'],
+                    'profile_picture' => $fetchdata['profile_picture'],
+                    'waktu' => $fetchdata['waktu'],
+                ];
+            }
+            echo json_encode($response);
+        }
+
+    }
